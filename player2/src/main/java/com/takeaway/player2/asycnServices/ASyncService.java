1281df87741aa8d3fa7896450a5c3d5142d73ca9
ahmed salah <ahmedm.salah.abdelrahman@gmail.com>
2018-01-24 00:59:07 +0200

    first commit

---
+++ player2/src/main/java/com/takeaway/player2/asycnServices/ASyncService.java
@@ -0,0 +1,32 @@
+package com.takeaway.player2.asycnServices;
+
+
+import com.takeaway.player2.configuration.GameProperties;
+import com.takeaway.player2.models.Pass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ASyncService {
+
+
+
+    @Autowired
+    GameProperties gameProperties;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Async
+    public void send(int  score) {
+        Pass p = new Pass();
+        p.setScore(score);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity httpRequest = new HttpEntity(p, headers);
+
+        // send the score over rest
+        ResponseEntity<String> response = restTemplate.exchange(gameProperties.getAnotherPlayerUrl()+ gameProperties.getPassUri(), HttpMethod.POST, httpRequest, String.class);
+    }
+}
---
+++ player2/src/main/java/com/takeaway/player2/controllers/RecieveController.java
@@ -0,0 +1,39 @@
+package com.takeaway.player2.controllers;
+
+import com.takeaway.player2.asycnServices.ASyncService;
+import com.takeaway.player2.models.Pass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("player2")
+public class RecieveController {
+
+    @Autowired
+    ASyncService aSyncService;
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/pass", method = RequestMethod.POST)
+    public void recieveScore(@RequestBody @Validated Pass passModel) {
+        System.out.println("received at player2");
+
+        if (passModel.getScore() % 3 == 0 && passModel.getScore() / 3 == 1) {
+            System.out.println("player 2 wins");
+        } else if (passModel.getScore() % 3 == 0) {
+            aSyncService.send(passModel.getScore()/3);
+        } else if (passModel.getScore() - 1 % 3 == 0) {
+            aSyncService.send(passModel.getScore() - 1);
+
+        } else {
+            aSyncService.send(passModel.getScore() + 1);
+        }
+    }
+
+}
